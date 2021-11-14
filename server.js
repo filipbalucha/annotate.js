b26c6a8ebffc4d05f0517507fb6fcfd129a43e18
Filip Balucha <balucha.filip2@gmail.com>
2021-11-14 02:07:50 -0500

    Set up file structure

---
+++ server.js
@@ -0,0 +1,10 @@
+"use strict";
+const path = require("path");
+const express = require("express");
+
+// Disclosure: This code has been adapted from the code used in Week 8 lectures.
+const app = express();
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, "/pub")));
