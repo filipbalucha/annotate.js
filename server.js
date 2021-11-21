5c79e5b0a13d3786bd6d009ce1ae34fa8bfb264c
Filip Balucha <balucha.filip2@gmail.com>
2021-11-20 21:35:56 -0500

    Serve HTML from root dir

--- server.js
+++ server.js
@@ -8,3 +8,7 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 app.use(express.static(path.join(__dirname, "/pub")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pub/examples.html"));
+});
