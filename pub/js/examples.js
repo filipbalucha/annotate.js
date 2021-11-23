bf27d4e107cfcd5c87b40035f2dc94b46b1cf63a
Filip Balucha <balucha.filip2@gmail.com>
2021-11-23 17:46:46 -0500

    Make demo webpage nicer and content dynamic

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -9,3 +9,19 @@ const colors = [
 ];
 
 new Annotate(colors);
+
+// Simulate growing content
+const paragraph =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum, mi eget vehicula sollicitudin, ex lacus vestibulum nibh, quis dignissim purus metus vitae orci. Donec ultricies cursus fringilla. Etiam at lacus ullamcorper, pulvinar dolor vel, convallis risus. Nullam condimentum condimentum euismod. Quisque blandit, dolor sed lobortis bibendum, nunc mauris feugiat turpis, sit amet suscipit est leo eget quam. Sed sit amet aliquet massa. Pellentesque commodo erat eget nisl aliquet, et ultricies tellus mollis. Aliquam pulvinar odio libero, ac vestibulum nibh auctor ac. Cras eu finibus ipsum. Nulla rhoncus rutrum congue. Donec non augue mollis, vulputate est et, porttitor quam. Morbi orci turpis, tempus ac sodales vitae, hendrerit id lacus. Nulla pharetra tincidunt tortor, in tincidunt felis auctor sed.";
+const sentences = paragraph.split(". ");
+
+let sentIdx = 0;
+const insertRow = () => {
+  const table = document.getElementById("growing-table");
+  const newRow = table.insertRow();
+  const cellLeft = newRow.insertCell();
+  const cellRight = newRow.insertCell();
+  cellLeft.innerHTML = sentences[sentIdx % sentences.length] + ".";
+  cellRight.innerHTML = sentences[sentIdx % sentences.length] + ".";
+  sentIdx++;
+};
